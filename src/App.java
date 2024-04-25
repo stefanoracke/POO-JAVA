a1e41254bcad50f0f30c3e396d01f18097c279ca
stefanoracke <stefanoracke@gmail.com>
2024-04-25 13:00:34 -0300

    commit

--- src/App.java
+++ src/App.java
@@ -1,9 +1,35 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int numero = 5;
-        int numero2 = 4;
+        String texto = "Esto es un texto asignado a una variable String";
+        System.out.println(texto);
 
-        System.out.println(numero + " + " + numero2 +" = "  + (numero + numero2) );
+        int longitud = texto.length();
+        System.out.println("La longitud de la cadena es: " + longitud);
 
+        char caracter = texto.charAt(8);
+        System.out.println("Caracter con charAt: " + caracter);
+
+        String subString = texto.substring(6, longitud);
+        System.out.println("Esto es una cadena acortada: " + subString);
+
+        String minusculo = texto.toLowerCase();
+        System.out.println("Texto pasado a minusculo: " + minusculo);
+
+        String mayuscula = texto.toUpperCase();
+        System.out.println("Texto pasado a mayuscula: " + mayuscula);
+
+        int indice = texto.indexOf("variable");
+        System.out.println(indice);
+
+        String remplazo = texto.replace("texto", "hola mundo");
+        System.out.println(remplazo);
+
+        boolean contain = texto.contains("asignado");
+        System.out.println("Has this text the word 'asignado'?: " + contain);
+
+        String textoConEspacio = "   Hola   ";
+        String textoSinEspacio = textoConEspacio.trim();
+        System.out.println(textoConEspacio);
+        System.out.println(textoSinEspacio);
     }
 }
