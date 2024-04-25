14263b6353fdc928fb50d932054d7136d2df4d08
stefanoracke <stefanoracke@gmail.com>
2024-04-24 23:39:38 -0300

    first

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numero = 5;
+        int numero2 = 4;
+
+        System.out.println(numero + " + " + numero2 +" = "  + (numero + numero2) );
+
+    }
+}
